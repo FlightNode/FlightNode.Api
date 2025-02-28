bd47838526c474102268a2641aed3c2537e26d5a
Stephen Fuqua <stephen@safnet.com>
2025-02-28 09:13:57 -0600

    Refactor to use variables

--- docker-scripts/test.sh
+++ docker-scripts/test.sh
@@ -6,7 +6,13 @@ pushd packages
 nuget install xunit.runner.console
 popd
 
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe test/FlightNode.Common.UnitTests/bin/Debug/FlightNode.Common.UnitTests.dll
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe test/FlightNode.DataCollection.UnitTests/bin/Debug/FlightNode.DataCollection.Domain.UnitTests.dll
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe test/FlightNode.Identity.UnitTests/bin/Debug/FlightNode.Identity.UnitTests.dll
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe test/FlightNode.Common.Api.UnitTests/bin/Debug/FligthNote.Common.Api.UnitTests.dll
+xunit=packages/xunit.runner.console.2.1.0/tools/xunit.console.exe
+common=FlightNode.Common.UnitTests
+dc=FlightNode.DataCollection.UnitTests
+identity=FlightNode.Identity.UnitTests
+api=FlightNode.Common.Api.UnitTests
+
+mono $xunit test/$common/bin/Debug/$common.dll
+mono $xunit test/$dc/bin/Debug/FlightNode.DataCollection.Domain.UnitTests.dll
+mono $xunit test/$identity/bin/Debug/$identity.dll
+mono $xunit test/$api/bin/Debug/FligthNote.Common.Api.UnitTests.dll
